ba1eb8f8041442a2cad176284981bb905e890dd0
abolfazl jazak <jazakabolfazl@gmail.com>
2024-12-12 02:03:12 +0330

    feat(swagger): add Swagger for API documentation
    - Introduced Swagger configuration in `src/config/swagger.config.ts` to document the Virgool backend API.
    - API documentation accessible at the `/swagger` route.
    - Updated project structure:
      - Moved `app` module to `src/modules/app/`.
      - Deleted old `app.controller.ts` and `app.service.ts` files.
      - Created new `app.controller.ts` and `app.service.ts` in the `app` module directory.
      - Adjusted imports and references to reflect the new structure.

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export function SwaggerConfigInit(app: INestApplication) {
+    const document = new DocumentBuilder()
+    .setTitle("virgool")
+    .setDescription("backEnd of virgool website")
+    .setVersion("v0.0.1")
+    .build()
+
+    const swaggerDocument = SwaggerModule.createDocument(app, document)
+    SwaggerModule.setup("/swagger", app, swaggerDocument)
+}
