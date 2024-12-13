831732dd991be56ed351c23e956fcc4de45e223a
abolfazl jazak <jazakabolfazl@gmail.com>
2024-12-13 16:26:04 +0330

    feat(config): integrate dotenv and typeorm configurations for environment management
    - Installed `@nestjs/config` and `dotenv` for environment variable management.
    - Added `env.d.ts` for strict TypeScript type definitions of environment variables.
      - Defines required variables for application and database configurations.
    - Created `typeorm.config.ts` to centralize and manage TypeORM configurations.
      - Extracts database credentials from environment variables.
      - Configured to auto-load entities and synchronize the database schema.
    - Updated `app.module.ts`:
      - Integrated `ConfigModule` globally with environment file support.
    - Removed unused `app.controller.ts` and `app.service.ts` for better modularity.

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 @Module({
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), "env")
+    })
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
