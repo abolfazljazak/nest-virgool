f4a13b93728501e7b9b7df9f4e7f18634a0ac26c
abolfazl jazak <jazakabolfazl@gmail.com>
2024-12-16 00:52:25 +0330

    feat(common): Add custom error messages for validation and authentication
    - Created `src/common/enum/message.enum.ts` to standardize error messages across the application.
    - Added enums for various categories:
      - `BadRequestMessage`: Messages for invalid login and registration data.
      - `AuthMessage`: Messages for account-related authentication scenarios.
      - `NotFoundMessage`: Placeholder for future "not found" messages.
      - `ValidationMessage`: Placeholder for future validation-specific messages.

---
+++ src/common/enum/message.enum.ts
@@ -0,0 +1,13 @@
+export enum BadRequestMessage {
+  InValidLoginData = "اصلاعات ارسال شده برای ورود صحیح نمیباشد",
+  InValidRegisterData = "اصلاعات ارسال شده برای ثبت نام صحیح نمیباشد",
+}
+
+export enum AuthMessage {
+  NotFoundAccount = "حساب کاربری یافت نشد",
+  AlreadyExistAccount = "حساب کاربری با این مشخصات قبلا ثبت شده",
+}
+
+export enum NotFoundMessage {}
+
+export enum ValidationMessage {}
