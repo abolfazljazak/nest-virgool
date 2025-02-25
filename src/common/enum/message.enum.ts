d65bd25e3fcb92e58e54bf9ac4f6eed16423f825
abolfazl jazak <jazakabolfazl@gmail.com>
2025-02-26 01:28:11 +0330

    feat(enum): Add new AuthMessage entries for expired code and retry prompts; correct PublicMessage entry for OTP sent

--- src/common/enum/message.enum.ts
+++ src/common/enum/message.enum.ts
@@ -6,6 +6,8 @@ export enum BadRequestMessage {
 export enum AuthMessage {
   NotFoundAccount = "حساب کاربری یافت نشد",
   AlreadyExistAccount = "حساب کاربری با این مشخصات قبلا ثبت شده",
+  ExpiredCode = "کد تایید منقضی شده لطفا مجدد تلاش کنید.",
+  TryAgain = "لطفا دوباره تلاش کنید.",
 }
 
 export enum NotFoundMessage {}
@@ -13,5 +15,5 @@ export enum NotFoundMessage {}
 export enum ValidationMessage {}
 
 export enum PublicMessage {
-  sentOtp = "کد یکبار مصرف با موفقیت ارسال شد"
+  sendOtp = "کد یکبار مصرف با موفقیت ارسال شد"
 }
