f24d2128de849ea0836612665aabfbfbd8b886aa
abolfazl jazak <jazakabolfazl@gmail.com>
2025-02-28 13:39:57 +0330

    feat(swagger): Enhance Swagger configuration to include JWT bearer authentication

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,13 +1,23 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SecuritySchemeObject } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
 
 export function SwaggerConfigInit(app: INestApplication) {
-    const document = new DocumentBuilder()
+  const document = new DocumentBuilder()
     .setTitle("virgool")
     .setDescription("backEnd of virgool website")
     .setVersion("v0.0.1")
-    .build()
+    .addBearerAuth(SwaggerAuthConfig())
+    .build();
 
-    const swaggerDocument = SwaggerModule.createDocument(app, document)
-    SwaggerModule.setup("/swagger", app, swaggerDocument)
+  const swaggerDocument = SwaggerModule.createDocument(app, document);
+  SwaggerModule.setup("/swagger", app, swaggerDocument);
+}
+function SwaggerAuthConfig(): SecuritySchemeObject {
+  return {
+    type: "http",
+    bearerFormat: "JWT",
+    in: "header",
+    scheme: "bearer",
+  };
 }
