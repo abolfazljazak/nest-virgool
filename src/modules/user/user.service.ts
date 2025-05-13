40bd749303b5c58764f9483e1dc3c2862bea7b30
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-13 17:27:25 +0330

    feat(user): implement changeProfile method in UserService for profile updates, including validation for bio, birthday, gender, and linkedIn fields

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,17 +1,53 @@
-import { Inject, Injectable, Scope } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserEntity } from './entities/user.entity';
-import { Repository } from 'typeorm';
-import { ProfileEntity } from './entities/profile.entity';
-import { REQUEST } from '@nestjs/core';
-import { Request } from 'express';
-import { ProfileDto } from './dto/profile.dto';
+import { Inject, Injectable, Scope } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "./entities/user.entity";
+import { Repository } from "typeorm";
+import { ProfileEntity } from "./entities/profile.entity";
+import { REQUEST } from "@nestjs/core";
+import { Request } from "express";
+import { ProfileDto } from "./dto/profile.dto";
+import { ApiBadGatewayResponse } from "@nestjs/swagger";
+import { isDate } from "class-validator";
+import { Gender } from "./enums/gender.enum";
 
 @Injectable({ scope: Scope.REQUEST })
 export class UserService {
-    constructor(
-        @InjectRepository(UserEntity) private userRepository: Repository<UserEntity>,
-        @InjectRepository(ProfileEntity) private profileRepository: Repository<ProfileEntity>,
-        @Inject(REQUEST) request: Request,
-    ) { }
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+    @InjectRepository(ProfileEntity)
+    private profileRepository: Repository<ProfileEntity>,
+    @Inject(REQUEST) private request: Request
+  ) {}
+
+  async changeProfile(files: any, profileDto: ProfileDto) {
+    const { id: userId, profileId } = this.request.user;
+    let profile = await this.profileRepository.findOneBy({ userId });
+    const { bio, birthday, gender, linkedIn, nick_name, x_profile } =
+      profileDto;
+    if (!profile) {
+        if (bio) profile.bio = bio
+        if (birthday && isDate(new Date(birthday))) profile.birthday = new Date(birthday) 
+        if (gender && Object.values(Gender as any).includes(gender)) profile.gender = gender
+        if (linkedIn) profile.linkedIn = linkedIn
+        if (x_profile) profile.x_profile = x_profile
+    } else {
+      this.profileRepository.create({
+        bio,
+        birthday,
+        gender,
+        linkedIn,
+        nick_name,
+        x_profile,
+        userId
+      });
+    }
+    await this.profileRepository.save(profile);
+    if (!profileId) {
+      await this.userRepository.update(
+        { id: userId },
+        { profileId: profile.id }
+      );
+    }
+  }
 }
