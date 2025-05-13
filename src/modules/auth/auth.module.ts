6710e2f85ed3e7aa691d3c0ff7aa152a1f1f865e
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-13 17:26:53 +0330

    feat(module): integrate TypeOrmModule in AuthModule and UserModule for entity management

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,13 +4,17 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtService } from '@nestjs/jwt';
 import { TokenService } from './tokens.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { OtpEntity } from '../user/entities/otp.entity';
+import { ProfileEntity } from '../user/entities/profile.entity';
 
 @Module({
   imports: [
-    UserModule
+    TypeOrmModule.forFeature([UserEntity, OtpEntity, ProfileEntity])
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtService, TokenService],
-  exports: [AuthService, JwtService, TokenService],
+  exports: [AuthService, JwtService, TokenService, TypeOrmModule],
 })
 export class AuthModule {}
