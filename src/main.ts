fd8678017b5185f5988423fc3839676a37b74994
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-13 18:10:12 +0330

    feat(main): enhance application bootstrap by adding static asset serving and updating NestFactory instantiation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
-import { SwaggerConfigInit } from './config/swagger.config';
-import * as CookieParser from 'cookie-parser'
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./modules/app/app.module";
+import { SwaggerConfigInit } from "./config/swagger.config";
+import * as CookieParser from "cookie-parser";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  SwaggerConfigInit(app)
-  app.use(CookieParser(process.env.COOKIE_SECRET))
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets("public");
+  SwaggerConfigInit(app);
+  app.use(CookieParser(process.env.COOKIE_SECRET));
 
   await app.listen(process.env.PORT ?? 3000);
 }
