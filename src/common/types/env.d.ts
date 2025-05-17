56e810befcb5bf5e4b0b7fb45440cf1f81c00c41
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-18 01:57:55 +0330

    feat(kavenegar): add KavenegarService for sending verification SMS and update environment types

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -10,11 +10,14 @@ namespace NodeJS {
         DB_USERNAME: string
         DB_PASSWORD: string
 
-        // secrets
+        // Secrets
         COOKIE_SECRET: string
         OTP_TOKEN_SECRET: string
         ACCESS_TOKEN_SECRET: string
         EMAIL_TOKEN_SECRET: string
         PHONE_TOKEN_SECRET: string
+
+        // Kavenegar
+        KAVENEGAR_SEND_SMS_URL: string
     }
 }
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from '../auth/auth.module';
 import { CategoryModule } from '../category/category.module';
 import { BlogModule } from '../blog/blog.module';
 import { ImageModule } from '../image/image.module';
+import { CustomHttpModule } from '../http/http.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -20,6 +21,7 @@ import { ImageModule } from '../image/image.module';
     CategoryModule,
     BlogModule,
     ImageModule,
+    CustomHttpModule,
   ],
   controllers: [],
   providers: []
