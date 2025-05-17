257ec57d104181a93bb819c8539e1532a8ab06f3
abolfazl jazak <jazakabolfazl@gmail.com>
2025-05-18 01:57:47 +0330

    feat(http): integrate @nestjs/axios and create CustomHttpModule for HTTP requests

---
+++ src/modules/http/http.module.ts
@@ -0,0 +1,14 @@
+import { HttpModule } from "@nestjs/axios";
+import { Module } from "@nestjs/common";
+import { KavenegarService } from "./kavenegar.service";
+
+@Module({
+  imports: [
+    HttpModule.register({
+      timeout: 10000,
+    }),
+  ],
+  providers: [KavenegarService],
+  exports: [KavenegarService],
+})
+export class CustomHttpModule {}
